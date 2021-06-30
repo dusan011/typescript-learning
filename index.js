ae15aef3102936d2b187d8455cec9b1f2bd2258c
Dusan Petrovic <dusanp93@live.com>
2021-06-30 12:18:04 +0200

    add dummy ts function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var firstName = 'muriel!';
+var firstName = "muriel!";
 console.log(firstName.toUpperCase());
 console.log(firstName.length);
+var a = 5;
+var b = 5;
+var multiply = function (a, b) {
+    return a * b;
+};
+console.log(multiply(a, b));
--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-let firstName : string = 'muriel!';
+let firstName: string = "muriel!";
 
 console.log(firstName.toUpperCase());
 
 console.log(firstName.length);
+
+const a: number = 5;
+const b: number = 5;
+
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+console.log(multiply(a, b));
