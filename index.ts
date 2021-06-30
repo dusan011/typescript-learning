8c3b82387f0c201d0a4e10666eb0143c55324284
Dusan Petrovic <dusanp93@live.com>
2021-06-30 12:19:37 +0200

    add dummy jsdoc

--- index.ts
+++ index.ts
@@ -7,6 +7,13 @@ console.log(firstName.length);
 const a: number = 5;
 const b: number = 5;
 
+/**
+ *
+ * This function multiplies two number
+ * @param a first number
+ * @param b second number
+ * @returns
+ */
 const multiply = (a: number, b: number): number => {
   return a * b;
 };
